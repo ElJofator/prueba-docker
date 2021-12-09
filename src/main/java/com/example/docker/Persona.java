0bd31b369423094125adb2e99fa34b82212984e4
Joseph <jofabacu64@gmail.com>
2021-12-09 11:21:32 -0500

    Añadir clase persona

---
+++ src/main/java/com/example/docker/Persona.java
@@ -0,0 +1,47 @@
+package com.example.docker;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+public class Persona {
+    @Id
+    private int id;
+    @Indexed
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Persona(int id, String nombre, String apellidos, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
